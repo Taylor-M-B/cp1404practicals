b784877065160e3310fbc0f92ff434ab2b19c506
jc508301 <taylor.buswell@my.jcu.edu.au>
2019-08-27 10:41:08 +1000

    Add random numbers program

---
+++ prac_02/random_numbers.py
@@ -0,0 +1,5 @@
+"""
+19 the smallest was 5 and largest was 20
+9 3 was the smallest 9 was the largest and no line 2 could not produce a 4
+2.708234166255245 2.5 was the smallest number that could be produced while 5.5 is the largest
+"""
