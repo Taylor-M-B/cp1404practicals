2a43ed4be63e439de4042cc202327d65bae1cf3e
Taylor <taylor.buswell@my.jcu.edu.au>
2019-08-28 10:20:14 +1000

    Add answers to list warm-up

--- prac_04/list_warmup.py
+++ prac_04/list_warmup.py
@@ -1,4 +1,17 @@
-"""pie """
+"""pie
+answers:
+3
+2
+1
+[3,1,4,1,5,9]
+[1]
+True
+False
+False
+[3,1,4,1,5,9,2,6,5,3]
+[4,1,5,9,1]
+True
+'ten',1,4,1,5,9,1]"""
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
 print(numbers[0])
@@ -10,4 +23,8 @@ print(5 in numbers)
 print(7 in numbers)
 print("3" in numbers)
 print(numbers + [6, 5, 3])
-"ten" = numbers.append(0)
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
+print(numbers)
